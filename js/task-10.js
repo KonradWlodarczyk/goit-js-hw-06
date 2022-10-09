7eeb08e575d15b4cedd0e9f8c313715db95fbe6d
Konrad Włodarczyk <draco320@gmail.com>
2022-10-09 13:18:11 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -10,24 +10,24 @@ const boxesCreate = document.querySelector("button[data-create]");
 const boxesDestroy = document.querySelector("button[data-destroy]");
 
 function createBoxes(amount) {
-  let heigth = 30;
+  let height = 30;
   let width = 30;
-  for (let i = 0; i <= amount; i++) {
+  for (let i = 0; i < amount; i++) {
     const box = document.createElement("div");
-    box.style.heigth = heigth + "px";
+    box.style.height = height + "px";
     box.style.width = width + "px";
     box.style.backgroundColor = getRandomHexColor();
     boxesContainer.appendChild(box);
-    heigth += 10;
+    height += 10;
     width += 10;
   }
 }
 function destroyBoxes() {
-  delete boxesContainer.childNodes;
+  boxesContainer.innerHTML = "";
 }
 let boxesAmount = "";
 boxesCounter.addEventListener("input", () => {
   boxesAmount = boxesCounter.value;
 });
-boxesCreate.addEventListener("click", createBoxes(boxesAmount));
+boxesCreate.addEventListener("click", () => createBoxes(boxesAmount));
 boxesDestroy.addEventListener("click", destroyBoxes);
